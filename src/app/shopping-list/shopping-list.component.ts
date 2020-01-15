2021fddd7c6890a3d8584764c05f7785e6ab21fa
Dmytro Marchenko <dmmarchenko@corelogic.com>
2020-01-15 12:34:49 +0200

    End of part 13

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ShoppingListService } from './shopping-list.service';
 import { Ingredient } from '../shared/ingredient.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
-export class ShoppingListComponent implements OnInit {
+export class ShoppingListComponent implements OnInit, OnDestroy {
 
   ingredients: Ingredient[] = [];
+  private subscription: Subscription;
 
   constructor(private shoppingListService: ShoppingListService) {
   }
 
   ngOnInit(): void {
     this.ingredients = this.shoppingListService.getItems();
-    this.shoppingListService.itemsChanged.subscribe((newIngredients: Ingredient[]) => {
+    this.subscription = this.shoppingListService.itemsChanged.subscribe((newIngredients: Ingredient[]) => {
       this.ingredients = newIngredients;
     });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
   }
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,8 @@
 import { Ingredient } from '../shared/ingredient.model';
-import { EventEmitter } from '@angular/core';
+import { Subject } from 'rxjs';
 
 export class ShoppingListService {
-  itemsChanged = new EventEmitter<Ingredient[]>();
+  itemsChanged = new Subject<Ingredient[]>();
   private items: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatoes', 10)
@@ -14,11 +14,11 @@ export class ShoppingListService {
 
   addItem(itemName: string, itemAmount: number) {
     this.items.push(new Ingredient(itemName, itemAmount));
-    this.itemsChanged.emit(this.items.slice());
+    this.itemsChanged.next(this.items.slice());
   }
 
   addItems(ingredients: Ingredient[]) {
     this.items.push(...ingredients);
-    this.itemsChanged.emit(this.items.slice());
+    this.itemsChanged.next(this.items.slice());
   }
 }
